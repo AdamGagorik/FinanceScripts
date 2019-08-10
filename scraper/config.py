ec98650e257f51ba67b072a2da148c2197fc74a4
Adam Gagorik <adam.gagorik@gmail.com>
2019-08-10 14:16:25 -0400

    Add methods to configure logging and pandas modules

--- scraper/config.py
+++ scraper/config.py
@@ -59,3 +59,22 @@ class Config:
         self.cookies = os.path.join(self.workdir, self.cookies)
         self.environ = os.path.join(self.workdir, self.environ)
         dotenv.load_dotenv(verbose=True, dotenv_path=self.environ)
+
+
+def pandas():
+    """
+    Set up the pandas module.
+    """
+    import pandas as _pandas
+    _pandas.set_option('display.max_colwidth', 1024)
+    _pandas.set_option('display.max_columns', 1024)
+    _pandas.set_option('display.max_rows', 1024)
+    _pandas.set_option('display.width', 4096)
+
+
+def logging():
+    """
+    Set up the logging module.
+    """
+    import logging as _logging
+    _logging.basicConfig(level=_logging.DEBUG, format='%(message)s')
