1bbdf15375afc3195724ef3d6b344ce7cedbebec
Adam Gagorik <adam.gagorik@gmail.com>
2019-12-23 15:04:41 -0500

    implement handler logic for YNAB API

--- scraper/handler.py
+++ scraper/handler.py
@@ -11,6 +11,9 @@ from personalcapital import RequireTwoFactorException
 from personalcapital import PersonalCapital
 
 
+import ynab_api as ynab
+
+
 from scraper.config import BaseConfig
 from scraper.config import YNABConfig
 from scraper.config import PCAPConfig
@@ -35,6 +38,50 @@ class YNABHandler(BaseHandler):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._api_client: typing.Union[ynab.ApiClient, None] = None
+        self._api_config: typing.Union[ynab.Configuration, None] = None
+        self._api_object: dict = {}
+
+    @property
+    def client(self) -> ynab.ApiClient:
+        """
+        Log into YNAB and save the session.
+        """
+        if self._api_config is None:
+            self._api_config: ynab.Configuration = ynab.Configuration()
+            self._api_config.api_key_prefix['Authorization'] = 'Bearer'
+            self._api_config.api_key['Authorization'] = self.config.ynab_apikey
+
+        if self._api_client is None:
+            self._api_client: ynab.ApiClient = ynab.ApiClient(self._api_config)
+            return self._api_client
+        else:
+            return self._api_client
+
+    def _get_api_object(self, key: str, klass: typing.Callable):
+        """
+        Fetch the API object or create and store it.
+        """
+        try:
+            return self._api_object[key]
+        except KeyError:
+            self._api_object[key] = klass(self.client)
+            return self._api_object[key]
+
+    @property
+    def budgets(self) -> ynab.BudgetsApi:
+        """Create or get existing API instance"""
+        return self._get_api_object('budgets', ynab.BudgetsApi)
+
+    @property
+    def accounts(self) -> ynab.AccountsApi:
+        """Create or get existing API instance"""
+        return self._get_api_object('accounts', ynab.AccountsApi)
+
+    @property
+    def transactions(self) -> ynab.TransactionsApi:
+        """Create or get existing API instance"""
+        return self._get_api_object('transactions', ynab.TransactionsApi)
 
 
 class PCAPHandler(BaseHandler):
