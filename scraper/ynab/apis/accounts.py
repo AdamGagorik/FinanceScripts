ae7438b87ac528014dc40ac8154ee35366d3c25a
Adam Gagorik <adam.gagorik@gmail.com>
2019-12-23 20:44:05 -0500

    Add budget-id as an option for accounts

--- scraper/ynab/apis/accounts.py
+++ scraper/ynab/apis/accounts.py
@@ -14,32 +14,19 @@ class Account(scraper.base.ObjectMapping):
     An object with account data.
     """
     # header
-    uuid: str = ''
+    id: str = ''
     name: str = ''
     type: str = ''
 
     # ammounts
     balance: int = 0
-    cleared: int = 0
-    pending: int = 0
+    cleared_balance: int = 0
+    uncleared_balance: int = 0
 
     # booleans
-    closed    : bool = False
-    deleted   : bool = False
-    on_budget : bool = True
-
-    # mapping from JSON
-    id                : dataclasses.InitVar[str] = ''
-    cleared_balance   : dataclasses.InitVar[int] = 0
-    uncleared_balance : dataclasses.InitVar[int] = 0
-
-    def __post_init__(self, id: str, cleared_balance: int, uncleared_balance: int):
-        """
-        Map the JSON keys to object.
-        """
-        self.uuid   : str = id
-        self.cleared: int = cleared_balance
-        self.pending: int = uncleared_balance
+    closed: bool = False
+    deleted: bool = False
+    on_budget: bool = True
 
 
 class AccountsScraper(scraper.base.YNABScraper):
@@ -50,6 +37,14 @@ class AccountsScraper(scraper.base.YNABScraper):
     __fillna_yaml__: str = 'fillna-ynab-accounts.yaml'
     __store_class__: type = Account
 
+    def __init__(self, *args, budget_id: str, **kwargs):
+        """
+        Parameters:
+            budget_id: The budget to fetch the accounts for.
+        """
+        self.budget_id: str = budget_id
+        super().__init__(*args, **kwargs)
+
     def fetch(self) -> list:
         """
         The logic of the API call.
@@ -57,7 +52,7 @@ class AccountsScraper(scraper.base.YNABScraper):
         Returns:
             The json dictionary.
         """
-        accounts: ynab.AccountResponse = self.handler.accounts.get_accounts('last-used')
+        accounts: ynab.AccountResponse = self.handler.accounts.get_accounts(self.budget_id)
 
         data: dict = accounts.to_dict().get('data', {})
         data: list = data.get('accounts', [])
--- scraper/ynab/apps/accounts.py
+++ scraper/ynab/apps/accounts.py
@@ -16,6 +16,7 @@ def get_arguments(args=None) -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('--force', action='store_true', help='force redownload?')
     parser.add_argument('--stub', default='{config.dt:%Y-%m-%d}-ynab-accounts.csv', type=str)
+    parser.add_argument('--budget-id', dest='budget_id', default='last-used', help='budget to fetch accounts for')
     return parser.parse_args(args=args)
 
 
